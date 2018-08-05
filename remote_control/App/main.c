64458e28ea59c0d6e33884c9b07976528a5a2e42
sumaoding <sumaoding@e1cdc6b9-7175-9a47-b313-2660960acd6d>
2018-08-05 03:06:01 +0000

    upload code base of remote control

---
+++ remote_control/App/main.c
@@ -0,0 +1,22 @@
+#include "stm32f10x.h"
+#include <stdio.h>
+
+
+
+int main(void)
+{
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       file (startup_stm32f10x_xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f10x.c file
+     */     
+
+  /* Add your application code here
+   */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
